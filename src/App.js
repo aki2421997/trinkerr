80cf660c992dfe4b573e72da0f34474fae8e1895
B0227466 <akshay.wanjari@gmail.com>
2022-02-19 11:01:55 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
-function App() {
+const BASE_URL = "http://3.109.141.224:5000";
+
+function debounce(cb,delay=300) {
+    let timer = null;
+    return function (...params) {
+        timer && clearTimeout(timer);
+        timer = setTimeout(()=>{
+            cb.apply(null,params);
+        },delay);
+    }
+}
+
+export default function App() {
+  const userToken = useRef(null);
+  const [data, setData] = useState(null);
+  useEffect(()=>{
+    (async function () {
+      try {
+        const res = await fetch(`${BASE_URL}/api/user-access-token`);
+        const {token} = await res.json();
+        userToken.current = token;
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  },[]);
+
+  const handleSearch = async (evt) => {
+    const val = evt.target.value;
+    console.log(val);
+    try {
+        const res = await fetch(`${BASE_URL}/api/data?search_string=${val}`,{
+            headers: {
+                'user-access-token': userToken.current
+            }
+        });
+        setData(await res.json());
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <input type="text" onChange={debounce(handleSearch,500)}/>
+      <div className="list">
+          {data?.map(item => {
+              return(
+                <div className="subList" key={item[0]}>
+                    {item.toString()}
+                </div>
+              )
+          })}
+      </div>
     </div>
   );
 }
-
-export default App;
